8b0b72c722a81dad777a31175b3d742795d90c57
randrianiainastanislasvola <randrianiainastanislasvola@gmail.com>
2023-07-17 12:17:52 +0300

    nagnisy commentaire

--- ProjetAina.ino
+++ ProjetAina.ino
@@ -1,30 +1,49 @@
-const int PCATEG = 4;// bouton poussoir mipetaky amin'ny pin 8
-const int DCATEG = 7;// bouton poussoir mipetaky amin'ny pin 7
-const int LEDHOMME = 2;// led mena mipetaky amin'ny pin 4
-const int LEDFEMME = 4;// led maintso mipetaky amin'ny pin 2
-int stats;
+// Constantes pour les broches du bouton et de la LED
+const int boutonPin = 2;//lkznlksdnzklfndslkzfnlkds
+const int ledPin = 13;
+
+// Variables pour le suivi de l'état du bouton et de la LED
+int boutonState = 0;
+int lastBoutonState = 0;
+int ledState = LOW;
+
+// Variable pour suivre le temps écoulé
+unsigned long startTime = 0;
+unsigned long elapsedTime = 0;
+const unsigned long dureeAllumee = 900000;  // 15 minutes en millisecondes
 
 void setup() {
-  pinMode(PCATEG, INPUT);// initialiser bouton poussoir au entree
-  pinMode(DCATEG, INPUT);
-  pinMode(LEDHOMME, OUTPUT);// initialiser  led au sorti
-  pinMode(LEDFEMME, OUTPUT);
-
+  // Configurer les broches d'entrée/sortie
+  pinMode(ledPin, OUTPUT);
+  pinMode(boutonPin, INPUT);
 }
 
 void loop() {
-  //stats = digitalRead(PCATEG);
-  //if(stats==LOW){
-    //digitalWrite(LEDHOMME, HIGH);// allumer le led de homme 
-    //delay(10000);
- // }
-  //else{
-    digitalWrite(LEDHOMME, LOW);    
-    delay(10000);
-    digitalWrite(LEDHOMME, LOW);
-    delay(10000);
+  // Lire l'état actuel du bouton
+  boutonState = digitalRead(boutonPin);
 
-  //}
- 
+  // Vérifier si le bouton est enfoncé
+  if (boutonState == HIGH && lastBoutonState == LOW) {
+    // Allumer la LED et enregistrer le temps de début
+    ledState = HIGH;
+    startTime = millis();
+  }
 
+  // Vérifier si la LED a été allumée
+  if (ledState == HIGH) {
+    // Calculer le temps écoulé depuis le début
+    elapsedTime = millis() - startTime;
+
+    // Vérifier si la durée allouée est écoulée
+    if (elapsedTime >= dureeAllumee) {
+      // Éteindre la LED
+      ledState = LOW;
+    }
+  }
+
+  // Mettre à jour l'état de la LED
+  digitalWrite(ledPin, ledState);
+
+  // Enregistrer l'état actuel du bouton pour la prochaine itération
+  lastBoutonState = boutonState;
 }
